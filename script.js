699170b9bc78127c18abf5a72917a437f207152f
edgariane <148069869+edgariane@users.noreply.github.com>
2024-01-08 14:50:41 +0100

    Update script.js
    Signed-off-by: edgariane <148069869+edgariane@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 document.cookie = "visited=yes";
+function getCookie(cname) {
+ let name = cname + "=";
+ let decodedCookie = decodeURIComponent(document.cookie);
+ let ca = decodedCookie.split(';');
+ for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+ }
+ return "";
+}
+
+function checkCookie() {
+ let user = getCookie("visited");
+ if (user != "") {
+    alert("Willkommen zurück!");
+ } else {
+    user = prompt("Bitte gib deinen Namen ein:", "");
+    if (user != "" && user != null) {
+      setCookie("visited", user, 30);
+    }
+ }
+}
+
+checkCookie();
